195493a239d3d30b7ae11ba989f440a457dfaa77
nabieltaqy <53118115+nabieltaqy@users.noreply.github.com>
2023-12-08 21:12:09 +0700

    FIX TOMBOL SEARCH

--- Laravel-Project/app/Http/Controllers/UserController.php
+++ Laravel-Project/app/Http/Controllers/UserController.php
@@ -10,10 +10,10 @@ class UserController extends Controller
     public function index(Request $request)
     {
         $keyword = $request->keyword;
-        dd($keyword);
+
         $users = User::where('fullname', 'LIKE','%'.$keyword.'%')
                         ->orWhere('username', 'LIKE','%'.$keyword.'%')
-                        ->orWhere('user_type', 'LIKE','%'.$keyword.'%')
+                        ->orWhere('user_type', 'LIKE', '%'.$keyword.'%')
                         ->paginate(5);
         return view('pengaturanadmin', ['users' => $users]);
     }
