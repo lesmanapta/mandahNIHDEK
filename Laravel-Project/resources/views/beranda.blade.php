b8e0747b18f791fade72e797dac788a17eab2e95
Sumanto Lesmana Putra <lesmana.pta@gmail.com>
2023-12-30 20:33:43 +0700

    push

--- Laravel-Project/resources/views/beranda.blade.php
+++ Laravel-Project/resources/views/beranda.blade.php
@@ -68,8 +68,21 @@
               <a href="/laporanpengeluaran" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
-    
 
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3>{{ $jumlahPelanggan }}</h3>
+
+                <p>Pelanggan</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-person-add"></i>
+              </div>
+              <a href="listKontak" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
         </div>
       </div><!-- /.container-fluid -->
     </section>
